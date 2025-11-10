68cb68753bd9fe7bd892d4e9e58364ea59ed9492
Salihxmod <semetsalih344@gmail.com>
2025-11-10 16:01:38 +0300

    Integrate Identity and domain entities into DbContext
    PromptixDbContext now inherits from IdentityDbContext with AppUser and AppRole entities. Added references to Domain project and Identity EntityFrameworkCore package in Infrastructure.csproj to support authentication and user management.

--- Promptix/Infrastructure/Data/PromptixDbContext.cs
+++ Promptix/Infrastructure/Data/PromptixDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace Infrastructure.Data;
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-public class PromptixDbContext
+namespace Infrastructure.Data;
+
+public class PromptixDbContext : IdentityDbContext<AppUser,AppRole,int>
 {
-    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
 }
