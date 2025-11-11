e5732058746dcbae7d5af21a343004d57925c388
Salihxmod <semetsalih344@gmail.com>
2025-11-11 20:54:14 +0300

    Add SQL Server support and new DbSets to DbContext
    Configured PromptixDbContext to use SQL Server and added DbSet properties for AuditLog, Category, Favorite, Payment, Prompt, PromptCategory, Purchase, and Subscription entities. Also added the Microsoft.EntityFrameworkCore.SqlServer package to the project.

--- Promptix/Infrastructure/Data/PromptixDbContext.cs
+++ Promptix/Infrastructure/Data/PromptixDbContext.cs
@@ -8,10 +8,22 @@ public class PromptixDbContext : IdentityDbContext<AppUser,AppRole,int>
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
+        optionsBuilder.UseSqlServer(
+            "Server=SQLEXPRESS;Database=PromptixDB;Trusted_Connection=True;"
+            );
         base.OnConfiguring(optionsBuilder);
     }
     protected override void OnModelCreating(ModelBuilder builder)
     {
+        
         base.OnModelCreating(builder);
     }
+    public DbSet<AuditLog> AuditLogs{ get; set; }
+    public DbSet<Category> Categories{ get; set; }
+    public DbSet<Favorite> Favorites{ get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<Prompt> Prompts { get; set; }
+    public DbSet<PromptCategory> PromptCategories { get; set; }
+    public DbSet<Purchase> Purchases { get; set; }
+    public DbSet<Subscription> Subscriptions { get; set; }
 }
