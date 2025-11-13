cea35cf509a26822840302cff658e4adc6061f02
Salihxmod <semetsalih344@gmail.com>
2025-11-13 12:39:33 +0300

    Implement Prompt entity configuration
    Added Entity Framework Core configuration for the Prompt entity, specifying table name, primary key, property constraints, default values, and an index on the Title property.

--- Promptix/Infrastructure/Configurations/PromptConfigurations.cs
+++ Promptix/Infrastructure/Configurations/PromptConfigurations.cs
@@ -1,6 +1,19 @@
-﻿namespace Infrastructure.Configurations;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class PromptConfigurations
+namespace Infrastructure.Configurations;
+
+public class PromptConfigurations : IEntityTypeConfiguration<Prompt>
 {
-    
+    public void Configure(EntityTypeBuilder<Prompt> builder)
+    {
+        builder.ToTable("Prompts");
+        builder.HasKey(x => x.Id); // Has Primary Key
+        builder.Property(p => p.Title).IsRequired().HasMaxLength(150);
+        builder.Property(x => x.Description).HasMaxLength(200);
+        builder.Property(x => x.Content).IsRequired();
+        builder.Property(x => x.Price).HasColumnName("decimal(18,2)").HasDefaultValue(0);
+        builder.HasIndex(p => p.Title);
+    }
 }
