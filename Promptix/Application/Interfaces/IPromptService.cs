b664c3612294406e7e2eac3a2fa639d5533691ce
Salihxmod <semetsalih344@gmail.com>
2025-11-13 18:06:51 +0300

    Add CRUD methods to IPromptService interface
    Introduced async CRUD operations to the IPromptService interface using Result and PromptDto types. This enhances the service contract to support create, read, update, and delete functionality for prompts.

--- Promptix/Application/Interfaces/IPromptService.cs
+++ Promptix/Application/Interfaces/IPromptService.cs
@@ -1,6 +1,12 @@
-﻿namespace Application.Interfaces;
+﻿using Application.DTO_s;
+using Application.Common;
+namespace Application.Interfaces;
 
 public interface IPromptService
 {
-    
+    Task<Result<IEnumerable<PromptDto>>> GetAllAsync();
+    Task<Result<PromptDto>> GetByIdAsync(int id);
+    Task<Result<PromptDto>> CreateAsync(PromptDto promptDto);
+    Task<Result<PromptDto>> UpdateAsync(PromptDto promptDto);
+    Task<Result<PromptDto>> DeleteAsync(PromptDto promptDto);
 }
