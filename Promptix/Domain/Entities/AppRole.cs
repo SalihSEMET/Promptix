da616665465b7f5834ba4bd0acf0fa9e6eb68bc5
Salihxmod <semetsalih344@gmail.com>
2025-11-13 13:07:54 +0300

    Add DbSeed class and extend AppRole with RoleType
    Introduces a DbSeed utility for seeding initial roles, categories, and prompts into the database. Also adds a RoleType property to the AppRole entity to support role categorization.

--- Promptix/Domain/Entities/AppRole.cs
+++ Promptix/Domain/Entities/AppRole.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Domain.Enums;
+using Microsoft.AspNetCore.Identity;
 
 namespace Domain.Entities;
 
 public class AppRole : IdentityRole<int>
 {
-    
+    public RoleType RoleType { get; set; }
 }
---
+++ Promptix/Infrastructure/Data/DbSeed.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Data;
+
+public static class DbSeed
+{
+    public static async Task SeedAsync(PromptixDbContext promptixDbContext)
+    {
+        if (promptixDbContext.Roles.Any())
+        {
+            promptixDbContext.Roles.AddRange(new[]
+            {
+                new AppRole { Name = "Admin", RoleType = Domain.Enums.RoleType.Admin },
+                new AppRole { Name = "UserApp", RoleType = Domain.Enums.RoleType.UserApp }
+            });
+            await promptixDbContext.SaveChangesAsync();
+        }
+
+        if (!promptixDbContext.Categories.Any())
+        {
+            var cat = new Category { Name = "AI Prompts" };
+            promptixDbContext.Categories.Add(cat);
+            await promptixDbContext.SaveChangesAsync();
+
+            var prompt = new Prompt
+            {
+                Title = "Email writer prompt",
+                Description = "Professional prompt printing with AI",
+                Content = "You are an export email writer...",
+                Price = 4.99m,
+            };
+
+            promptixDbContext.Prompts.AddAsync(prompt);
+            promptixDbContext.SaveChangesAsync();
+            
+            var pCat = new PromptCategory { PromptId = prompt.Id, CategoryId = cat.Id };
+            promptixDbContext.PromptCategories.AddAsync(pCat);
+            promptixDbContext.SaveChangesAsync();
+
+        }
+    }
+}
