20f9e55ee44a93ee8631159c3b79e6613174434f
Salihxmod <semetsalih344@gmail.com>
2025-11-14 15:48:54 +0300

    Add Id property to IBaseEntity and enhance repository queries
    Introduced a public Id property to IBaseEntity for consistent entity identification. Updated Repository methods to support filtering, ordering, and including related entities in GetAllAsync and FindAsync, improving query flexibility and data retrieval.

--- Promptix/Infrastructure/Repositories/Repository.cs
+++ Promptix/Infrastructure/Repositories/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Domain.Enums;
 using Domain.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -16,9 +17,33 @@ public class Repository<T> : IRepository<T> where T : class,IBaseEntity, new()
         DbSet = context.Set<T>();
     }
     public async Task<T?> GetByIdAsync(int id) => await DbSet.FindAsync(id);
-    public async Task<IEnumerable<T>> GetAllAsync() => await DbSet.ToListAsync();
-    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate) =>
-        await DbSet.Where(predicate).ToListAsync();
+    public async Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>> filter = null ,OrderType orderBy = OrderType.Asc,params string[] include)
+    {
+        IQueryable<T> query = DbSet.AsQueryable();
+        if (filter != null)
+            query = query.Where(filter);
+        foreach (var item in include)
+        {
+            query = query.Include(item);
+        }
+        
+        query = orderBy == OrderType.Asc ? query.OrderBy(x => x.Id) : query.OrderByDescending(x => x.Id);
+        
+        return await DbSet.ToListAsync();
+    }
+
+    public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate,OrderType orderBy = OrderType.Asc,params string[] include)
+    {
+        IQueryable<T> query = DbSet.AsQueryable(); //In Query Status: Our query has not yet been sent to the database. Used during the query preparation phase.
+        query = query.Where(predicate);
+        foreach (var item in include)
+        {
+            query = query.Include(item);
+        }
+        query = orderBy == OrderType.Asc ? query.OrderBy(x => x.Id) : query.OrderByDescending(x => x.Id);
+        
+        return await DbSet.ToListAsync(); //The query is sent to the database, executed, and the results are returned.
+    }
     public async Task AddAsync(T entity) => await DbSet.AddAsync(entity);
     public async Task AddRangeAsync(IEnumerable<T> entities) => await DbSet.AddRangeAsync(entities);
     public void Update(T entity) => DbSet.Update(entity);
