8fce05342d427a00541b4f120762fc235d0991f5
Salihxmod <semetsalih344@gmail.com>
2025-11-14 12:29:30 +0300

    Add OrderType enum and update IRepository methods
    Introduced the OrderType enum to specify ordering direction. Updated IRepository interface methods to support ordering and including related entities in GetAllAsync and FindAsync.

--- Promptix/Domain/Interfaces/IRepository.cs
+++ Promptix/Domain/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Domain.Enums;
 
 namespace Domain.Interfaces;
 
@@ -9,8 +10,8 @@ public interface IRepository<T> where T : class, IBaseEntity, new()
     // that is, in a type-independent manner,
     // and creating methods that can be used for all entities as well as all tables. 
     Task<T?> GetByIdAsync(int id);
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<IEnumerable<T>> FindAsync(Expression<Func<T,bool>> predicate);
+    Task<IEnumerable<T>> GetAllAsync(Expression<Func<T,bool>> filter = null ,OrderType orderBy = OrderType.Asc,params string[] include);
+    Task<IEnumerable<T>> FindAsync(Expression<Func<T,bool>> predicate,OrderType orderBy = OrderType.Asc,params string[] include);
     Task AddAsync(T entity);
     Task AddRangeAsync(IEnumerable<T> entities);
     void Update(T entity);
